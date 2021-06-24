6bc76de55e9ddcae4bb3a2a393df92fa28d0b394
Noah <sovannarasay11@gmail.com>
2021-06-24 13:13:22 -0600

    Created the UI for the app.

--- MadLibsApp/MadLibsApp/MainPage.xaml.cs
+++ MadLibsApp/MadLibsApp/MainPage.xaml.cs
@@ -10,6 +10,10 @@ namespace MadLibsApp
 {
     public partial class MainPage : ContentPage
     {
+        string story = "Hi my name is Tom Holland, but my friends call me fun whippersnapper. " +
+            "My favorite color is the color of scissors and my favorite thing to do is dance. " +
+            "My parents were a cheetah and baker, which is why we lived in theater. " +
+            "You probably know me from my TV commercial for soda. I'm the one who says, 'Life is like a box of chocolates' at the very end!";
         public MainPage()
         {
             InitializeComponent();
